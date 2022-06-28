04b8c900bd4f4c125193a8fb39429778ee40d2a6
Thamires Amancio <thamyamancio17@gmail.com>
2022-06-28 18:00:01 -0300

    finish

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import {useQuery, gql } from "@apollo/client";
 import Lesson from "./Lesson";
 
 const GET_LESSONS_QUERY = gql`
@@ -11,7 +11,6 @@ const GET_LESSONS_QUERY = gql`
     title
   }
 }
-
 `
 
 interface GetLessonQueryResponse{
@@ -23,9 +22,9 @@ interface GetLessonQueryResponse{
     lessonType:'live'|'class'
   }[]
 }
+
 export default function Sidebar(){
   const { data } = useQuery<GetLessonQueryResponse>(GET_LESSONS_QUERY)
-
   return(
     <aside className="w-[348px] bg-gray-700 p-6 border-gray-600 ">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">Cronomograna de aulas</span>
